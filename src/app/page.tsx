6a5b2c1c87c9584f8bcb27f03bc4e41c7887f510
byt3h3ad <adhirajd007@gmail.com>
2024-11-01 20:38:39 +0530

    yoloing at 2024-11-01T20.38

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,12 @@
 
 import { Stories } from "@/components/Stories";
 import { useGetToday } from "@/hooks/useGetToday";
-import { dummy_data } from "@/utils/constants";
 
 export default function Home() {
   const { data, isLoading, isError } = useGetToday();
-
   return (
     <>
-      <h1 className="text-3xl font-bold">{dummy_data.heading}</h1>
+      <h1 className="text-3xl font-bold">{data.heading}</h1>
       {isLoading && <p>Loading...</p>}
       {isError && <p>Something went wrong</p>}
       <Stories data={data.links} />
